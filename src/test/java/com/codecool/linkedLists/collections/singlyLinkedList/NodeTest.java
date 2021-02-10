34525d2c1f21d1b610d186e1154931c010a3803f
Adam Sallai <yadam1998@gmail.com>
2021-02-10 18:01:41 +0100

    add test to Node getValue method

---
+++ src/test/java/com/codecool/linkedLists/collections/singlyLinkedList/NodeTest.java
@@ -0,0 +1,17 @@
+package com.codecool.linkedLists.collections.singlyLinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NodeTest {
+
+	@Test
+	public void testNodeValue() {
+		Node<String> nameNode = new Node<String>("Adam");
+		Node<Integer> numberNode = new Node<Integer>(23);
+		
+		assertEquals("Adam", nameNode.getValue());
+		assertEquals(23, numberNode.getValue());
+	}
+}
