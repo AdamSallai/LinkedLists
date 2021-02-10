109c1e82b08152fd92aa6bc546097cf4bbd98169
Adam Sallai <yadam1998@gmail.com>
2021-02-10 16:40:38 +0100

    add SinglyLinkedList class that implements LinkedList interface

---
+++ src/main/java/com/codecool/linkedLists/collections/SinglyLinkedList.java
@@ -0,0 +1,40 @@
+package com.codecool.linkedLists.collections;
+
+public class SinglyLinkedList<T> implements LinkedList<T>{
+
+	public void add(T value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void remove(T value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public T removeByIndex(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean contains(T value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int indexOf(T value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
